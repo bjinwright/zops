c21ce03cbc9dbad5083ed4009b8697e794c3486b
Brian Jinwright <bjinwright@qwigo.com>
2016-09-08 18:22:27 -0400

    Added delete_user and undeploy_initial commands

--- zops/__init__.py
+++ zops/__init__.py
@@ -4,30 +4,31 @@ import subprocess
 
 import awacs
 import boto3
-from unipath import Path
-from awacs.aws import Allow, Policy, Statement
-from awacs.s3 import ARN as S3_ARN
-from awacs.iam import ARN as IAM_ARN
 from awacs.apigateway import ARN as APIGW_ARN
+from awacs.aws import Allow, Policy, Statement
 from awacs.awslambda import ARN as LAMBDA_ARN
+from awacs.iam import ARN as IAM_ARN
+from awacs.s3 import ARN as S3_ARN
+from awacs.events import ARN as EVENTS_ARN
 from jinja2 import FileSystemLoader, Environment
-
 from troposphere import GetAtt, Output, Ref, Template
 from troposphere.iam import PolicyType, User, AccessKey
+from unipath import Path
 
 
 class Zops(object):
 
     def __init__(self, app_name, stage_name,
-                 function_bucket=None, static_bucket=None,username=None,aws_region_name=None):
+                 function_bucket=None, static_bucket=None,
+                 username=None, aws_region_name=None):
         self.app_name = app_name
         self.stage_name = stage_name
-        self.user_stack_name = 'zappa{0}{1}user'.format(self.app_name,self.stage_name)
+        self.user_stack_name = 'zappa{0}{1}user'.format(
+            self.app_name, self.stage_name)
         self.function_bucket = function_bucket
         self.static_bucket = static_bucket
         self.aws_region_name = aws_region_name
-        self.username = username or 'zappa{0}{1}user'.format(
-            self.app_name,self.stage_name)
+        self.username = username or self.user_stack_name
         self.t = Template()
         self.t.add_description(
             "Zappa Template for {app_name}-{stage_name} ".format(
@@ -40,58 +41,75 @@ class Zops(object):
         stm_list.extend(self.create_iam_policy())
         stm_list.extend(self.create_apigw_policy())
         stm_list.extend(self.create_lambda_policy())
+        stm_list.extend(self.create_events_policy())
         return stm_list
 
-    def create_user_stack(self,username,function_bucket,static_bucket,aws_region_name):
-        if function_bucket:
-            self.function_bucket = function_bucket
-        if static_bucket:
-            self.static_bucket = static_bucket
-
-        self.aws_region_name = aws_region_name or 'us-east-1'
+    def create_user_stack(self):
+        #Create the CloudFormation stack
         return self.cf.create_stack(
-            StackName=username or self.username,
-            TemplateBody=self.render_user(username),
+            StackName=self.user_stack_name,
+            TemplateBody=self.render_user(),
             Capabilities=['CAPABILITY_IAM']
         )
 
-    def user_stack_outputs(self,username):
-        return self.cf.Stack(name=username).outputs
+    def user_stack_outputs(self):
+        #Get the Cloudformation user stack's outputs
+        return self.cf.Stack(name=self.user_stack_name).outputs
 
-    def create_initial_app(self,function_bucket,aws_region_name='us-east-1'):
-        if function_bucket:
-            self.function_bucket = function_bucket
-        self.aws_region_name = aws_region_name
+    def delete_user_stack(self):
+        return self.cf.Stack(name=self.user_stack_name).delete()
 
+    def create_initial_app(self):
+        #Create app directory
         os.makedirs('app')
         template_path = Path(__file__).ancestor(1).child('templates')
-
-        env = Environment(autoescape=False,loader=FileSystemLoader(template_path))
+        #Create Jinja2 Environment
+        env = Environment(autoescape=False,
+                          loader=FileSystemLoader(template_path))
+        #Get flask and zappa_settings templates
         flask_app_template = env.get_template('flask.py.jinja2')
-        zappa_settings_template = env.get_template('zappa_settings.json.jinja2')
-        with open('app/{app_name}.py'.format(app_name=self.app_name),'w+') as f:
-            f.write(flask_app_template.render(app_name=self.app_name,stage_name=self.stage_name))
+        zappa_settings_template = env.get_template(
+            'zappa_settings.json.jinja2')
+        #Create Flask app and zappa_settings.json files in the app directory
+        with open('app/{app_name}.py'.format(app_name=self.app_name), 'w+') as f:
+            f.write(flask_app_template.render(app_name=self.app_name, stage_name=self.stage_name))
         with open('app/zappa_settings.json'.format(app_name=self.app_name), 'w+') as f:
             f.write(zappa_settings_template.render(app_name=self.app_name,
                                                    stage_name=self.stage_name,
                                                    function_bucket=self.function_bucket,
-                                                   aws_region_name=aws_region_name))
-        shutil.copytree(template_path.child('templates'),'app/templates')
+                                                   aws_region_name=self.aws_region_name))
+        #Copy the HTML template to the app/templates directory
+        shutil.copytree(template_path.child('templates'), 'app/templates')
 
     def deploy_initial_app(self):
-        creds = self.user_stack_outputs(self.username)
+        #Get the user stack's keys
+        creds = self.user_stack_outputs()
         os.environ['AWS_SECRET_ACCESS_KEY'] = creds[0]['OutputValue']
         os.environ['AWS_ACCESS_KEY_ID'] = creds[1]['OutputValue']
         try:
+            #Run zappa deploy command
             subprocess.check_output(['zappa', 'deploy', self.stage_name, '-s', 'app/zappa_settings.json'])
         except Exception as e:
+            #If there is an error with the zappa deploy command print the output
+            print(e.output)
+
+    def undeploy_initial_app(self):
+        # Get the user stack's keys
+        creds = self.user_stack_outputs()
+        os.environ['AWS_SECRET_ACCESS_KEY'] = creds[0]['OutputValue']
+        os.environ['AWS_ACCESS_KEY_ID'] = creds[1]['OutputValue']
+        try:
+            # Run zappa deploy command
+            subprocess.check_output(['zappa', 'undeploy', self.stage_name, '-y','-s', 'app/zappa_settings.json'])
+        except Exception as e:
+            # If there is an error with the zappa deploy command print the output
             print(e.output)
 
     def delete_initial_app(self):
         shutil.rmtree('app')
 
-    def render_user(self,username):
-        zappa_user = self.t.add_resource(User(username or self.username))
+    def render_user(self):
+        zappa_user = self.t.add_resource(User(self.username))
         zappa_user_keys = self.t.add_resource(AccessKey(
             "ZappaUserKeys",
             Status="Active",
@@ -137,9 +155,9 @@ class Zops(object):
             bucket_list_paths.extend([S3_ARN("{0}/{1}-{2}-static/*".format(
                 self.static_bucket,
                 self.app_name, self.stage_name)),
-            S3_ARN("{0}/{1}-{2}-media/*".format(
-                self.static_bucket,
-                self.app_name, self.stage_name)),])
+                S3_ARN("{0}/{1}-{2}-media/*".format(
+                    self.static_bucket,
+                    self.app_name, self.stage_name)), ])
 
         return [
             Statement(
@@ -219,12 +237,15 @@ class Zops(object):
                     awacs.awslambda.AddPermission,
                     awacs.awslambda.DeleteFunction,
                     awacs.awslambda.GetFunction,
+                    awacs.awslambda.GetPolicy,
                     awacs.awslambda.ListVersionsByFunction,
-                    awacs.awslambda.UpdateFunctionCode
+                    awacs.awslambda.UpdateFunctionCode,
+                    awacs.awslambda.RemovePermission
+
                 ],
                 Resource=[
                     LAMBDA_ARN('{0}-{1}'.format(
-                        self.app_name, self.stage_name))
+                        self.app_name, self.stage_name),region=self.aws_region_name,account='*')
                 ]
 
             ),
@@ -238,4 +259,33 @@ class Zops(object):
                 ]
 
             ),
+        ]
+
+    def create_events_policy(self):
+        return [
+            Statement(
+                Effect=Allow,
+                Action=[
+                    awacs.events.ListRules,
+                ],
+                Resource=[
+                    EVENTS_ARN('rule/*',region=self.aws_region_name, account='*')
+                ]
+            ),
+            Statement(
+                Effect=Allow,
+                Action=[
+                    awacs.events.PutRule,
+                    awacs.events.PutTargets,
+                    awacs.events.ListTargetsByRule,
+                    awacs.events.DeleteRule,
+                    awacs.events.RemoveTargets
+                ],
+                Resource=[
+                    EVENTS_ARN(
+                        resource='rule/{app_name}-{stage_name}-zappa-keep-warm-handler.keep_warm_callback'.format(
+                            app_name=self.app_name,stage_name=self.stage_name),
+                        region=self.aws_region_name, account='*')
+                ]
+            )
         ]
--- zops/cli.py
+++ zops/cli.py
@@ -3,29 +3,31 @@ import terminaltables
 from zops import Zops
 
 @click.group()
+def zops():
+    return
+
+@zops.command()
 @click.argument('app_name')
 @click.argument('stage_name')
-@click.pass_context
-def zops(ctx,app_name,stage_name):
-    ctx.obj['app_name'] = app_name
-    ctx.obj['stage_name'] = stage_name
-    ctx.obj['zops'] = Zops(app_name,stage_name)
-
-@zops.command()
-@click.pass_context
-@click.option('--username',default=None)
+@click.option('--username',default=None,prompt='Username',help='Username for IAM user')
 @click.option('--function_bucket',prompt='Function Bucket',help="Bucket for the function's code.")
-@click.option('--static_bucket',prompt='Static Bucket',default=None,help='Bucket for your static assets.')
-@click.option('--aws_region_name',prompt='AWS Region Name',default='us-east-1',help='AWS Region Name')
-def create_user(ctx,username,function_bucket,static_bucket,aws_region_name):
-    click.echo('Creating user: {0}'.format(username or ctx.obj['zops'].username))
-    return ctx.obj['zops'].create_user_stack(username,function_bucket,static_bucket,aws_region_name)
+@click.option('--static_bucket',prompt='Static Bucket',default=None,
+              help='Bucket for your static assets.')
+@click.option('--aws_region_name',prompt='AWS Region Name',
+              default='us-east-1',help='AWS Region Name')
+def create_user(app_name,stage_name,username,function_bucket,static_bucket,
+                aws_region_name):
+    z = Zops(app_name,stage_name,function_bucket=function_bucket,
+                static_bucket=static_bucket,username=username,
+                aws_region_name=aws_region_name)
+    click.echo('Creating user: {0}'.format(username or zops.username))
+    return z.create_user_stack()
 
 @zops.command()
-@click.pass_context
-@click.option('--username',prompt='Username?')
-def user_credentials(ctx,username):
-    result = ctx.obj['zops'].user_stack_outputs(username)
+@click.argument('app_name')
+@click.argument('stage_name')
+def user_credentials(app_name,stage_name):
+    result = Zops(app_name,stage_name).user_stack_outputs()
     try:
         table_data = [
             [i['OutputKey'], i['OutputValue']]
@@ -39,16 +41,42 @@ def user_credentials(ctx,username):
     click.echo(table.table)
 
 @zops.command()
+@click.argument('app_name')
+@click.argument('stage_name')
+def delete_user(app_name,stage_name):
+
+    z = Zops(app_name,stage_name)
+    z.delete_user_stack()
+    click.echo('Deleted user stack: {0}'.format(z.user_stack_name), color="red")
+
+
+@zops.command()
+@click.argument('app_name')
+@click.argument('stage_name')
 @click.option('--function_bucket',prompt='Function Bucket',help="Bucket for the function's code.")
-@click.option('--aws_region_name',prompt='AWS Region Name',help="AWS Region Name ")
-@click.pass_context
-def deploy_initial(ctx,function_bucket,aws_region_name):
+@click.option('--aws_region_name',prompt='AWS Region Name',default='us-east-1',help="AWS Region Name ")
+def deploy_initial(app_name,stage_name,function_bucket,aws_region_name):
+    z = Zops(app_name,stage_name,function_bucket=function_bucket,aws_region_name=aws_region_name)
     click.echo('Creating initial app...')
-    ctx.obj['zops'].create_initial_app(function_bucket,aws_region_name)
+    z.create_initial_app()
     click.echo('Deploying initial app...')
-    ctx.obj['zops'].deploy_initial_app()
+    z.deploy_initial_app()
     click.echo('Deleting local copy of initial app...')
-    ctx.obj['zops'].delete_initial_app()
+    z.delete_initial_app()
+
+@zops.command()
+@click.argument('app_name')
+@click.argument('stage_name')
+@click.option('--function_bucket',prompt='Function Bucket',help="Bucket for the function's code.")
+@click.option('--aws_region_name',prompt='AWS Region Name',default='us-east-1',help="AWS Region Name ")
+def undeploy_initial(app_name,stage_name,function_bucket,aws_region_name):
+    z = Zops(app_name,stage_name,function_bucket=function_bucket,aws_region_name=aws_region_name)
+    click.echo('Creating initial app...')
+    z.create_initial_app()
+    click.echo('Undeploying initial app...')
+    z.undeploy_initial_app()
+    click.echo('Deleting local copy of initial app...')
+    z.delete_initial_app()
 
 
-zops_ins = zops(obj={})
+zops_ins = zops()
